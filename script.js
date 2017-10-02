5f670e7172a291dc6dff0474e89da0338758e248
DESKTOP-CP2B0BD\Mathias Vansant <mathias.vansant@immalle.be>
2017-10-02 12:59:18 +0200

    Initial commit: added site-files

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 var howOften = 4; // 4 seconds
-var current = Math.floor(Math.random() * 5);
+var current = Math.floor(Math.random() * 7);
 var ns6 = document.getElementById&&!document.all;
 var items = new Array()
-	items[0]="<a href='#'><img alt='tuin' src='afb/tuinfoto3.jpg' width='60%' height='500' border='0'/></a>";
-	items[1]="<a href='#'><img alt='tuin' src='afb/tuinfotodonker1.jpg' width='60%' height='500' border='0' /></a>";
-	items[2]="<a href='#'><img alt='tuin' src='afb/tuinfoto2.jpg' width='60%' height='500' border='0' /></a>";
-	items[3]="<a href='#'><img alt='binnen' src='afb/binnen1.jpg' width='60%' height='500' border='0' /></a>";
-	items[4]="<a href='#'><img alt='gevel' src='afb/gevel1.jpg' width='60%' height='500' border='0' /></a>";
+	items[0]="<img alt='' src='' width='96%' border='0'/>";
+	items[1]="<img alt='' src='' width='96%' border='0' />";
+	items[2]="<img alt='' src='' width='96%' border='0' />";
+	items[3]="<img alt='' src='' width='96%' border='0' />";
+	items[4]="<img alt='' src='' width='96%' border='0' />";
+	items[5]="<img class='verticalfoto' alt='' src='' width='55%' border='0' />";
+	items[6]="<img class='verticalfoto' alt='' src='' width='70%' border='0' />";
 function rotater() {
 	document.getElementById("placeholder").innerHTML = items[current];
 	current = (current==items.length-1) ? 0 : current + 1;
